2ce988f655ec8c005cdc89881fad2795219da17a
Charlie McDonald <c.mcdonald@berkeley.edu>
2024-05-09 23:02:14 -0700

    compiler issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,20 +46,7 @@ const config: Config = {
     },
   },
   plugins: [
-    plugin(function({ addUtilities }) {
-      addUtilities({
-        /* Hide scrollbar for Chrome, Safari and Opera */
-        '.no-scrollbar::-webkit-scrollbar': {
-            'display': 'none',
-          },
-  
-          /* Hide scrollbar for IE, Edge and Firefox */
-          '.no-scrollbar': {
-            '-ms-overflow-style': 'none',  /* IE and Edge */
-            'scrollbar-width': 'none',  /* Firefox */
-          }
-      })
-    })
+    
   ]
 };
 export default config;
